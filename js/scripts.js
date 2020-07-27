26e448e6d252e29746b4c50051fa8024a7598e98
annaadhiambo <annaadhiambo01@gmail.com>
2020-07-27 13:35:56 +0300

    added a mailchip in index.html,styled and used scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -28,4 +28,24 @@ $(document).ready(function(){
         $('.product-text').toggle(2000);
     });
 });
+$(document).ready(function(){
+    $('#work1').mouseover(function(){
+        $('#top1').show();
+    }).mouseout(function(){
+        $('#top1').hide();
+    })
+
+});
+$(document).ready(function(){
+    $('form#validity').submit(function(){
+        var name = $('input#smile1').val();
+        var email = $('input#smile2').val();
+        var message = $('input#textarea').val();
+        if (name && email){
+            alert (name + ', we have recived your message. Thank you for contacting us ');
+        }else {
+            alert('Please enter your name and email');
+        }
+    });
+});
 
